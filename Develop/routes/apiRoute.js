e8e2f9b156e3c93eb84135fffe71df9e5133b2ad
Todey <chloe.e.hanks@gmail.com>
2023-01-04 18:00:38 -0600

    added comments to files to explain functionality

--- Develop/routes/apiRoute.js
+++ Develop/routes/apiRoute.js
@@ -1,9 +1,12 @@
 // Import modules
+// create new router object to handle requests
 const apiRouter = require('express').Router();
+// require the db.json file for getting and sending data
 const data = require('../db/db.json');
+// pull createNote and deleteNote from the middleware/helpers.js file
 const { createNote, deleteNote } = require('../middleware/helpers');
 
-// Get all saved notes
+// Get all saved notes from the db.json file
 apiRouter.get('/notes', (req, res) => {
     res.send(data);
 });
@@ -13,16 +16,17 @@ apiRouter.get('/', (req, res) => {
     res.send(`Here is the API endpoint`);
 });
 
-// POST a new note to the db
+// POST a new note to the db.json file
 apiRouter.post('/notes', (req, res) => {
     createNote(req.body);
     res.json();
 });
 
-// Delete a note by id from the db
+// Delete a note by its unique id from the db.json file
 apiRouter.delete('/notes/:id', (req, res) => {
     deleteNote(req.params);
     res.json();
 });
 
+// make this file a module to call elsewhere (i.e. server.js)
 module.exports = apiRouter;
